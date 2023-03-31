bb86120df37550e7bd3d0f47dac6a2417e9c4292
murali sai <nanisai039@gmail.com>
2023-03-31 20:00:20 +0530

    Add project files.

---
+++ BookStore/Repositories/Abstract/IAuthorService.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Models.Domain;
+
+namespace BookStore.Repositories.Abstract
+{
+    public interface IAuthorService
+    {
+        bool Add(Author model);
+        bool Update(Author model);
+        bool Delete(int id);
+
+        Author FindById(int id);
+
+        IEnumerable<Author> GetAll();
+    }
+}
---
+++ BookStore/Repositories/Abstract/IBookService.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Models.Domain;
+
+namespace BookStore.Repositories.Abstract
+{
+    public interface IBookService
+    {
+        bool Add(Book model);
+        bool Update(Book model);
+        bool Delete(int id);
+
+        Book FindById(int id);
+
+        IEnumerable<Book> GetAll();
+    }
+}
